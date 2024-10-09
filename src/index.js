2346719e9d063b4a220f74fc5e53b7aedb69ea1e
Siarhei <errorcoderecords@gmail.com>
2024-10-09 15:40:45 +0200

    updt

--- src/index.js
+++ src/index.js
@@ -14,4 +14,21 @@ const swiper = new Swiper('.slider-teacher', {
         prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
-  });
+});
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var panel = this.nextElementSibling;
+   if (panel.style.maxHeight) {
+     panel.style.maxHeight = null;
+   } else {
+     panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+ });
+}
+
+  
